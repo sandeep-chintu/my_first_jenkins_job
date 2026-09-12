4b5fdf21b3b4419638c3f96e2b55eb94127d06c2
sandeep-chintu <165257000+sandeep-chintu@users.noreply.github.com>
2026-09-12 13:13:11 +0530

    Correct password check and improve string reversal
    Fixed password check logic and cleaned up code.

--- p1.py
+++ p1.py
@@ -1,31 +1,36 @@
+# checking weather the password is correct or not?
+==================================================================================================
 password=input("enter the password: ")
 if password = chintu:
     print("successfully logged........")
 else:
     print("enter correct password>>>?")
+==================================================================================================
+# reverse the string?
+==================================================================================================
 str="sandeep is a good boy"
 str1=" "
 for i in str:
     if i[::-1]:
         str1=i+str1
 print(str1)
-
-name="hello world"
-print(name)
-
+==================================================================================================
+# checking weather the password is correct or not?
+==================================================================================================
 password="sandeep"
 if password == password:
     print("entered correct password")
 else:
     print("entered incorrect password")
-
-
-
+=================================================================================================
+# checking the vote elgibility
+=================================================================================================
 age=int(input("enter the name: "))
 if age>17:
     print("man or wommen is eligibile to vote")
 else:
     print("not elgibile for vote")
+    
 
 
 
