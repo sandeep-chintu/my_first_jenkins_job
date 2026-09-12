a79dc145931b4367d45a6a856a9e9eb04e08bd76
sandeep <sandeepnagendla3@gmail.com>
2026-09-12 07:34:53 +0000

    Merged p2.py into p1.py and removed p2.py

--- p1.py
+++ p1.py
@@ -3,3 +3,30 @@ if password = chintu:
     print("successfully logged........")
 else:
     print("enter correct password>>>?")
+str="sandeep is a good boy"
+str1=" "
+for i in str:
+    if i[::-1]:
+        str1=i+str1
+print(str1)
+
+name="hello world"
+print(name)
+
+password="sandeep"
+if password == password:
+    print("entered correct password")
+else:
+    print("entered incorrect password")
+
+
+
+age=int(input("enter the name: "))
+if age>17:
+    print("man or wommen is eligibile to vote")
+else:
+    print("not elgibile for vote")
+
+
+
+
